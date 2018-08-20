6eb749cd1c0eb24554a6346b7452def0382b2b85
Amy Cohen <anbcohen@gmail.com>
2018-08-20 06:47:46 -0700

    continuing to follow through with the video and input the methods discussed in class.

--- src/main/java/AdjacencyListGraph.java
+++ src/main/java/AdjacencyListGraph.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class AdjacencyListGraph<E> implements Graph<E> {
     //From Video
-    Map<Node<E>, List<Node<E>>> adjacencyList;
+    Map<Node<E>, Set<Node<E>>> adjacencyList;
+    Map<Node<E>, Map<Node<E>, Integer>> costs;
 
     public AdjacencyListGraph() {
         //From Video
@@ -18,11 +19,13 @@ public class AdjacencyListGraph<E> implements Graph<E> {
         return null;
     }
 
-    //add a new empty node to the adjacency list and give it an empty array list so it can have a placeholder for possible edges
+    //add a new empty node to the adjacency list and give it an empty array list so it can have a placeholder for possible connections
+    //add a way to track the costs also by adding a new empty hash map on the node to via costs
     @Override
     public void addNode(Node<E> node) {
         //From Video
-        adjacencyList.put(node, new ArrayList<>());
+        adjacencyList.put(node, new HashSet<>());
+        costs.put(node, new HashMap<>());
     }
 
     @Override
@@ -31,27 +34,33 @@ public class AdjacencyListGraph<E> implements Graph<E> {
         adjacencyList.get(start).add(end);
     }
 
+    //add a new edge, then use costs to get the start of that edge. It will have a map associated with it that you can then call to put both the end of the edge and the cost associated with it.
     @Override
     public void addEdge(Node<E> start, Node<E> end, int cost) {
         //From Video
-        adjacencyList.get(start).add(end);
-
+        addEdge(start, end);
+        Map<Node<E>, Integer> options = costs.get(start);
+        options.put(end, cost);
     }
 
     @Override
     public void addTwoWayEdge(Node<E> start, Node<E> end) {
-        adjacencyList.get(start).add(end);
-        adjacencyList.get(end).add(start);
+        //From Video
+        addEdge(start, end);
+        addEdge(end, start);
     }
 
     @Override
     public void addTwoWayEdge(Node<E> start, Node<E> end, int cost) {
+        //From Video
+        addEdge(start, end, cost);
+        addEdge(end, start, cost);
     }
 
     @Override
     public Set<Node<E>> getNeighbors(Node<E> node) {
-        return null;
-
+        //From Video
+        return adjacencyList.get(node);
     }
 
     @Override
