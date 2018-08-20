1ea07eac656ba203c5622b3da1c36f997fd730f4
Amy Cohen <anbcohen@gmail.com>
2018-08-20 06:25:34 -0700

    starting to follow through with the video and input the methods discussed in class.

--- src/main/java/AdjacencyListGraph.java
+++ src/main/java/AdjacencyListGraph.java
@@ -1,32 +1,47 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class AdjacencyListGraph<E> implements Graph<E> {
+    //From Video
+    Map<Node<E>, List<Node<E>>> adjacencyList;
+
     public AdjacencyListGraph() {
+        //From Video
+        adjacencyList = new HashMap<>();
     }
 
     public Set<Node<E>> getNodes() {
+        //From Video
+        return adjacencyList.keySet();
     }
 
     public Set<Edge<E>> getEdges() {
+        return null;
     }
 
+    //add a new empty node to the adjacency list and give it an empty array list so it can have a placeholder for possible edges
     @Override
     public void addNode(Node<E> node) {
+        //From Video
+        adjacencyList.put(node, new ArrayList<>());
     }
 
     @Override
     public void addEdge(Node<E> start, Node<E> end) {
+        //From Video
+        adjacencyList.get(start).add(end);
     }
 
     @Override
     public void addEdge(Node<E> start, Node<E> end, int cost) {
+        //From Video
+        adjacencyList.get(start).add(end);
+
     }
 
     @Override
     public void addTwoWayEdge(Node<E> start, Node<E> end) {
+        adjacencyList.get(start).add(end);
+        adjacencyList.get(end).add(start);
     }
 
     @Override
@@ -35,14 +50,19 @@ public class AdjacencyListGraph<E> implements Graph<E> {
 
     @Override
     public Set<Node<E>> getNeighbors(Node<E> node) {
+        return null;
+
     }
 
     @Override
     public boolean isConnected(Node<E> start, Node<E> end) {
+        return false;
     }
 
     @Override
     public Edge<E> getEdge(Node<E> start, Node<E> end) {
+        return null;
+
     }
 
     private void checkNodesExists(Node<E> node1, Node<E> node2) {
